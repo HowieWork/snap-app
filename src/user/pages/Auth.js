d76fccc0d628d70d1c64d93e02d773e2ec1fc490
Howie <howiewang2020@gmail.com>
2021-11-03 17:19:52 -0400

    Add App-wide state management

---
+++ src/user/pages/Auth.js
@@ -0,0 +1,112 @@
+import { useState, useContext } from 'react';
+
+import Card from '../../shared/components/UIElements/Card';
+import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
+import { useForm } from '../../shared/hooks/form-hook';
+import { AuthContext } from '../../shared/context/auth-context';
+
+import './Auth.css';
+import {
+  VALIDATOR_EMAIL,
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from '../../shared/util/validators';
+
+const Auth = () => {
+  const auth = useContext(AuthContext);
+  const [isLoginMode, setIsLoginMode] = useState(true);
+  const [formState, inputHandler, setFormData] = useForm(
+    {
+      email: {
+        value: '',
+        isValid: false,
+      },
+      password: {
+        value: '',
+        isValid: false,
+      },
+    },
+    false
+  );
+
+  const switchModeHandler = () => {
+    if (!isLoginMode) {
+      setFormData(
+        { ...formState.inputs, name: undefined },
+        formState.inputs.email.isValid && formState.inputs.password.isValid
+      );
+    } else {
+      setFormData(
+        {
+          ...formState.inputs,
+          name: {
+            value: '',
+            isValid: false,
+          },
+        },
+        false
+      );
+    }
+    setIsLoginMode((prevMode) => !prevMode);
+  };
+
+  const authSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(formState.inputs);
+    auth.login();
+  };
+
+  return (
+    <Card className='authentication-form'>
+      <h2 className='authentication-form-header'>Login Required</h2>
+      <form onSubmit={authSubmitHandler}>
+        {!isLoginMode && (
+          <Input
+            element='input'
+            id='name'
+            type='text'
+            label='Name'
+            validators={[VALIDATOR_REQUIRE()]}
+            errorText='Please enter a name.'
+            onInput={inputHandler}
+          />
+        )}
+        <Input
+          element='input'
+          id='email'
+          type='email'
+          label='Email'
+          validators={[VALIDATOR_EMAIL()]}
+          errorText='Please enter a valid email address.'
+          onInput={inputHandler}
+        />
+        <Input
+          element='input'
+          id='password'
+          type='password'
+          label='Password'
+          validators={[VALIDATOR_MINLENGTH(8)]}
+          errorText='Please enter a valid password, at least 5 characters.'
+          onInput={inputHandler}
+        />
+        <div className='authentication-form-actions'>
+          <Button type='submit' disabled={!formState.isValid}>
+            {isLoginMode ? 'Login' : 'Signup'}
+          </Button>
+          {isLoginMode ? (
+            <Button onClick={switchModeHandler} inverse>
+              Join now
+            </Button>
+          ) : (
+            <Button onClick={switchModeHandler} inverse>
+              Switch to login
+            </Button>
+          )}
+        </div>
+      </form>
+    </Card>
+  );
+};
+
+export default Auth;
