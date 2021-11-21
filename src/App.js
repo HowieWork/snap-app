a8950a5707653e4e7225816c7dc51812cd601f50
Howie <howiewang2020@gmail.com>
2021-11-21 11:26:59 -0500

    Create custom authentication hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useState, useCallback, useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -13,74 +12,10 @@ import UserSnaps from './snap/pages/UserSnaps';
 import NewSnap from './snap/pages/NewSnap';
 import UpdateSnap from './snap/pages/UpdateSnap';
 import { AuthContext } from './shared/context/auth-context';
-
-let logoutTimer;
+import { useAuth } from './shared/hooks/auth-hook';
 
 const App = () => {
-  // NOTE ISLOGGEDIN IS REPLACED BY WHETHER TOKEN EXISTS
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [token, setToken] = useState(null);
-  const [tokenExpirationDate, setTokenExpirationDate] = useState();
-  const [userId, setUserId] = useState(null);
-
-  const login = useCallback((uid, token, expirationDate) => {
-    // REACT WILL BATCH ALL THE FOLLOWING SET STATE TOGETHER
-    setToken(token);
-    setUserId(uid);
-
-    // NOTE STORE TOKEN IN LOCAL STORAGE
-    // PASS EXISTING EXPIRATION DATE OR GENERATE A DATE OBJECT: NOW + 1 HOUR
-    const tokenExpirationDate =
-      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
-
-    setTokenExpirationDate(tokenExpirationDate);
-
-    localStorage.setItem(
-      'userData',
-      JSON.stringify({
-        userId: uid,
-        token: token,
-        // ISO STRING CAN BE CONVERTED BACK TO A DATE OBJECT
-        expiration: tokenExpirationDate.toISOString(),
-      })
-    );
-  }, []);
-
-  const logout = useCallback(() => {
-    setToken(null);
-    setUserId(null);
-    // REMORE USERDATA STORED LOCALLY
-    localStorage.removeItem('userData');
-  }, []);
-
-  // IMPLEMENT AUTO-LOGOUT
-  useEffect(() => {
-    if (token && tokenExpirationDate) {
-      const remainingTime =
-        tokenExpirationDate.getTime() - new Date().getTime();
-      logoutTimer = setTimeout(logout, remainingTime);
-    }
-    if (!token || !tokenExpirationDate) {
-      clearTimeout(logoutTimer);
-    }
-  }, [token, logout, tokenExpirationDate]);
-
-  // IMPLEMENT AUTO-LOGIN
-  useEffect(() => {
-    // TODO(OPTIONAL) FIX 'HICCUP' WHEN FIRST RENDER IS NOT LOGGED IN
-    const storedData = JSON.parse(localStorage.getItem('userData'));
-    if (
-      storedData &&
-      storedData.token &&
-      new Date(storedData.expiration) > new Date()
-    ) {
-      login(
-        storedData.userId,
-        storedData.token,
-        new Date(storedData.expirationDate)
-      );
-    }
-  }, [login]);
+  const { token, login, logout, userId } = useAuth();
 
   let routes;
 
