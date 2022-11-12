16cb7bfe3b6b220164787441ae8296eefdc070ed
Howie <howiewang2020@gmail.com>
2022-11-12 09:54:06 -0500

    update footer

--- src/shared/components/Footer/Footer.js
+++ src/shared/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className='footer-section center-text'>
-      © 2021 Howie Wang. All rights reserved.
+      © 2022 Howie Wang. All rights reserved.
     </footer>
   );
 };
